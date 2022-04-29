2e35ecb86a1fd8ec663d82c9d037a200a23835d4
Hugo T R <hugo@linux.com>
2022-04-29 07:43:06 +0200

    new modules: uuid, zerolog/hlog and quicktemplate

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/nanzhong/tstr
 go 1.18
 
 require (
+	github.com/google/uuid v1.1.2
 	github.com/jackc/pgconn v1.12.0
 	github.com/jackc/pgtype v1.11.0
 	github.com/jackc/pgx/v4 v4.16.0
@@ -10,6 +11,7 @@ require (
 	github.com/rs/zerolog v1.26.1
 	github.com/spf13/cobra v1.4.0
 	github.com/spf13/viper v1.11.0
+	github.com/valyala/quicktemplate v1.7.0
 	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c
 	google.golang.org/grpc v1.46.0
 	google.golang.org/protobuf v1.28.0
@@ -30,11 +32,13 @@ require (
 	github.com/mitchellh/mapstructure v1.4.3 // indirect
 	github.com/pelletier/go-toml v1.9.4 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.0-beta.8 // indirect
+	github.com/rs/xid v1.3.0 // indirect
 	github.com/spf13/afero v1.8.2 // indirect
 	github.com/spf13/cast v1.4.1 // indirect
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/subosito/gotenv v1.2.0 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	golang.org/x/crypto v0.0.0-20220411220226-7b82a4e95df4 // indirect
 	golang.org/x/net v0.0.0-20220412020605-290c469a71a5 // indirect
 	golang.org/x/sys v0.0.0-20220412211240-33da011f77ad // indirect
---
+++ webui/routes.go
@@ -0,0 +1,71 @@
+package webui
+
+import (
+	"net/http"
+
+	"github.com/nanzhong/tstr/webui/templates"
+	"github.com/rs/zerolog/hlog"
+)
+
+//go:generate qtc -dir=templates
+
+type WebUI struct{}
+
+func NewWebUI() *WebUI {
+	return &WebUI{}
+}
+
+func (ui *WebUI) ServeDashboard(w http.ResponseWriter, r *http.Request) {
+	hlog.FromRequest(r).Info().Msg("serving dashboard")
+	p := &templates.Dashboard{}
+	templates.WritePageTemplate(w, p)
+}
+
+func (ui *WebUI) ServeRuns(w http.ResponseWriter, r *http.Request) {
+
+	hlog.FromRequest(r).Info().Msg("serving runs page")
+	runs := genRuns()
+	hasPendingRuns := false
+	hasFinishedRuns := false
+
+	for _, run := range runs {
+		if run.FinishedAt == nil {
+			hasPendingRuns = true
+			break
+		} else {
+			hasFinishedRuns = true
+		}
+	}
+
+	p := &templates.RunsPage{
+		Runs:            runs,
+		HasPendingRuns:  hasPendingRuns,
+		HasFinishedRuns: hasFinishedRuns,
+	}
+	templates.WritePageTemplate(w, p)
+}
+
+func (w *WebUI) Handler() http.Handler {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", w.ServeDashboard)
+
+	mux.HandleFunc("/runs", w.ServeRuns)
+
+	mux.HandleFunc("/labels", func(w http.ResponseWriter, r *http.Request) {
+		hlog.FromRequest(r).Info().Msg("serving labels page")
+
+		lbl1 := templates.LabelSummary{
+			Label:       "xlabel: zvalue",
+			MonthReport: genDayReports(60),
+		}
+
+		monthsummary := []templates.LabelSummary{lbl1}
+
+		p := &templates.LabelsPage{monthsummary}
+		templates.WritePageTemplate(w, p)
+	})
+
+	// TODO: fix me. please.
+	return hlog.MethodHandler("method")(hlog.URLHandler("url")(hlog.RemoteAddrHandler("peer")(mux)))
+}

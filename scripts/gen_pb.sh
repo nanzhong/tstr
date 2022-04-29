c9d7cc1381617d589a7e4234374eb0661de8280a
Nan Zhong <me@nanzho.ng>
2022-04-29 15:28:17 -0400

    Add validation for RegisterTest rpc

--- scripts/gen_pb.sh
+++ scripts/gen_pb.sh
@@ -3,8 +3,12 @@
 shopt -s globstar
 
 protoc \
+    --proto_path=. \
+    --proto_path=vendor/github.com/envoyproxy/protoc-gen-validate \
     --go_out=. \
     --go_opt=paths=source_relative \
     --go-grpc_out=. \
     --go-grpc_opt=paths=source_relative \
-    **/*.proto
+    --validate_out="lang=go:." \
+    --validate_opt="paths=source_relative" \
+    api/**/*.proto

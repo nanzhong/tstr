44ca0df90f24336f4c3f828c9fd1b2623ed5e9b9
Nan Zhong <me@nanzho.ng>
2022-09-22 07:34:09 -0400

    Remove use of .env

--- dev/common.sh
+++ dev/common.sh
@@ -2,5 +2,8 @@
 
 set -euo pipefail
 
+export DATABASE_URL="postgres://tstr@127.0.0.1:5432/tstr_development?sslmode=disable"
+export TEST_DATABASE_URL="postgres://tstr@127.0.0.1:5432/tstr_test?sslmode=disable"
+
 export dev_state_path=".dev"
 export dev_overmind_sock="$dev_state_path/overmind.sock"

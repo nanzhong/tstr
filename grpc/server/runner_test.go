a1d6254cc470e2cdbef3a45391606f5cd65fbdcd
catalin mihale <v-cmihale@digitalocean.com>
2022-11-01 09:35:24 +0200

    Fixed register runner unit test

--- grpc/server/runner_test.go
+++ grpc/server/runner_test.go
@@ -2,21 +2,24 @@ package server
 
 import (
 	"context"
-	"fmt"
 	"testing"
+	"time"
 
 	"github.com/golang/mock/gomock"
 	"github.com/google/uuid"
 	"github.com/jackc/pgtype"
+	commonv1 "github.com/nanzhong/tstr/api/common/v1"
 	runnerv1 "github.com/nanzhong/tstr/api/runner/v1"
 	"github.com/nanzhong/tstr/db"
 	"github.com/nanzhong/tstr/grpc/auth"
 	"github.com/nanzhong/tstr/grpc/types"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/metadata"
 )
 
-func newTestRunnerServer(t *testing.T) (*RunnerServer, *db.MockQuerier){
+func newTestRunnerServer(t *testing.T) (*RunnerServer, *db.MockQuerier) {
 	ctrl := gomock.NewController(t)
 	t.Cleanup(ctrl.Finish)
 	mockQuerier := db.NewMockQuerier(ctrl)
@@ -26,77 +29,96 @@ func newTestRunnerServer(t *testing.T) (*RunnerServer, *db.MockQuerier){
 	}, mockQuerier
 }
 
+func createRunnerData(namespaceSelectors []string) *commonv1.Runner {
+	runnerName := "runner"
+	runnerId, _ := uuid.Parse(runnerName)
+	timestamp := types.ToProtoTimestamp(time.Now())
+	return &commonv1.Runner{
+		Id:                       runnerId.String(),
+		Name:                     runnerName,
+		NamespaceSelectors:       namespaceSelectors,
+		AcceptTestLabelSelectors: map[string]string{"region": "nyc"},
+		RejectTestLabelSelectors: map[string]string{"region": "syd"},
+		RegisteredAt:             timestamp,
+		LastHeartbeatAt:          timestamp,
+	}
+}
 
-func TestRunnerServer_ResgisterRunner(t *testing.T) {
-	/*tests := []struct {
-		name				string
-		responseCode		codes.Code
-		errMsg				string
-		mockQuerierReturn	func(*commonv1.Runner) (db.RegisterRunnerParams, error)
+func convertTestLabelSelectors(testLabelSelectors map[string]string) pgtype.JSONB {
+	var transformedLabel pgtype.JSONB
+	transformedLabel.Set(testLabelSelectors)
+	return transformedLabel
+}
 
+func TestRunnerServer_RegisterRunner(t *testing.T) {
+	tests := []struct {
+		name                    string
+		responseCode            codes.Code
+		errMsg                  string
+		token                   *commonv1.AccessToken
+		runner                  func([]string) *commonv1.Runner
+		mockQuerierReturnAuth   func(*commonv1.AccessToken) (db.AuthAccessTokenRow, error)
+		mockQuerierReturnRunner func(*commonv1.Runner) (db.Runner, error)
 	}{
 		{
-		name: "",
-		responseCode: code.OK,
-		errMsg: "",
-		mockQuerierReturn: func(*commonv1.Runner) (db.RegisterRunnerParams, error){
-			token, _ := s.dbQuerier.AuthAccessToken(ctx, s.pgxPool, tokenHash)
-
-			return commonv1.Runner{
-				Id:                       regRunner.ID.String(),
-				Name:                     regRunner.Name,
-				NamespaceSelectors:       regRunner.NamespaceSelectors,
-				AcceptTestLabelSelectors: acceptSelectors,
-				RejectTestLabelSelectors: rejectSelectors,
-				RegisteredAt:             types.ToProtoTimestamp(regRunner.RegisteredAt),
-				LastHeartbeatAt:          types.ToProtoTimestamp(regRunner.LastHeartbeatAt),
-
-			} 
-		}
-		*/
+			name:         "valid",
+			responseCode: codes.OK,
+			errMsg:       "",
+			token:        newAccessTokenBuilder().build(),
+			runner: func(namespaceSelectors []string) *commonv1.Runner {
+				return createRunnerData(namespaceSelectors)
+			},
+			mockQuerierReturnAuth: func(token *commonv1.AccessToken) (db.AuthAccessTokenRow, error) {
+				tokenID, _ := uuid.Parse(token.Id)
+				return db.AuthAccessTokenRow{
+					ID:                 tokenID,
+					Name:               token.Name,
+					NamespaceSelectors: token.NamespaceSelectors,
+					Scopes:             []string{"admin"},
+					IssuedAt:           types.FromProtoTimestampAsNullTime(token.IssuedAt),
+					ExpiresAt:          types.FromProtoTimestampAsNullTime(token.ExpiresAt),
+					RevokedAt:          types.FromProtoTimestampAsNullTime(token.RevokedAt),
+				}, nil
+			},
+			mockQuerierReturnRunner: func(runner *commonv1.Runner) (db.Runner, error) {
+				runnerID, _ := uuid.Parse(runner.Id)
+				return db.Runner{
+					ID:                       runnerID,
+					Name:                     runner.Name,
+					NamespaceSelectors:       runner.NamespaceSelectors,
+					AcceptTestLabelSelectors: convertTestLabelSelectors(runner.AcceptTestLabelSelectors),
+					RejectTestLabelSelectors: convertTestLabelSelectors(runner.RejectTestLabelSelectors),
+					RegisteredAt:             types.FromProtoTimestampAsNullTime(runner.RegisteredAt),
+					LastHeartbeatAt:          types.FromProtoTimestampAsNullTime(runner.LastHeartbeatAt),
+				}, nil
+			},
+		},
+	}
 
 	server, mockQuerier := newTestRunnerServer(t)
-	tokenString := "token"
-	ctx := metadata.NewIncomingContext(context.Background(), metadata.Pairs(auth.MDKeyAuth, "bearer "+tokenString))
-	token := newAccessTokenBuilder().withRevokedAt().build()
-	runnerName := "runner"
-	runnerId, _ := uuid.Parse(runnerName) 
-	tokenID, _ := uuid.Parse(token.Id)
-	tokenHash := auth.HashToken(tokenString)
 
-	var (
-		accept pgtype.JSONB
-		reject pgtype.JSONB
-		acceptSelectors pgtype.JSONB
-		rejectSelectors pgtype.JSONB
-	)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ctx := metadata.NewIncomingContext(context.Background(), metadata.Pairs(auth.MDKeyAuth, "bearer "+tt.token.Name))
+			tokenHash := auth.HashToken(tt.token.Name)
+			mockQuerier.EXPECT().AuthAccessToken(ctx, gomock.AssignableToTypeOf(server.pgxPool), tokenHash).Return(tt.mockQuerierReturnAuth(tt.token))
 
-	mockQuerier.EXPECT().AuthAccessToken(ctx, gomock.AssignableToTypeOf(server.pgxPool), tokenHash).Return(db.AuthAccessTokenRow{
-		ID:                 tokenID,
-		Name:               token.Name,
-		NamespaceSelectors: token.NamespaceSelectors,
-		Scopes:             []string{"admin"},
-		IssuedAt:           types.FromProtoTimestampAsNullTime(token.IssuedAt),
-		ExpiresAt:          types.FromProtoTimestampAsNullTime(token.ExpiresAt),
-	}, nil)
+			runner := tt.runner(tt.token.NamespaceSelectors)
+			mockQuerier.EXPECT().RegisterRunner(ctx, gomock.AssignableToTypeOf(server.pgxPool), db.RegisterRunnerParams{
+				Name:                     runner.Name,
+				NamespaceSelectors:       tt.token.NamespaceSelectors,
+				AcceptTestLabelSelectors: convertTestLabelSelectors(runner.AcceptTestLabelSelectors),
+				RejectTestLabelSelectors: convertTestLabelSelectors(runner.RejectTestLabelSelectors),
+			}).Return(tt.mockQuerierReturnRunner(runner))
 
-	mockQuerier.EXPECT().RegisterRunner(ctx, gomock.AssignableToTypeOf(server.pgxPool), db.RegisterRunnerParams{
-		Name:                     runnerName,
-		NamespaceSelectors:       token.NamespaceSelectors,
-		AcceptTestLabelSelectors: accept,
-		RejectTestLabelSelectors: reject,
-	}).Return(db.Runner{
-		ID:                       runnerId,
-		Name:                     runnerName,
-		NamespaceSelectors:       token.NamespaceSelectors,
-		AcceptTestLabelSelectors: acceptSelectors,
-		RejectTestLabelSelectors: rejectSelectors,
-		RegisteredAt:             types.FromProtoTimestampAsNullTime(token.IssuedAt),
-		LastHeartbeatAt:          types.FromProtoTimestampAsNullTime(token.IssuedAt),
-	}, nil)
+			request := &runnerv1.RegisterRunnerRequest{
+				Name:                     runner.Name,
+				AcceptTestLabelSelectors: runner.AcceptTestLabelSelectors,
+				RejectTestLabelSelectors: runner.RejectTestLabelSelectors}
+			res, err := server.RegisterRunner(ctx, request)
 
-	request := &runnerv1.RegisterRunnerRequest{Name: "TestRunner"}
-	res , err := server.RegisterRunner(ctx, request)
-	require.NoError(t, err)
-	fmt.Println(res)
+			assert.Equal(t, &runnerv1.RegisterRunnerResponse{Runner: runner}, res)
+			require.NoError(t, err)
+		})
+	}
 }

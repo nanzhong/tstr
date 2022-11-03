b0b36913102f8db3e419ae7709f994ef61f58bc8
catalin mihale <v-cmihale@digitalocean.com>
2022-11-03 13:14:11 +0200

    Fixed negative test scenarios

--- grpc/server/runner_test.go
+++ grpc/server/runner_test.go
@@ -63,7 +63,7 @@ func TestRunnerServer_RegisterRunner(t *testing.T) {
 		mockQuerierReturnRunner func(*commonv1.Runner) (db.Runner, error)
 	}{
 		{
-			name:         "valid",
+			name:         "valid access token available when registering runner",
 			responseCode: codes.OK,
 			errMsg:       "",
 			token:        newAccessTokenBuilder().build(),
@@ -96,31 +96,22 @@ func TestRunnerServer_RegisterRunner(t *testing.T) {
 			},
 		},
 		{
-			name:         "Authortize token returns error",
+			name:         "fail query for auth access token",
 			responseCode: codes.Internal,
 			errMsg:       "failed to get identity",
 			token:        newAccessTokenBuilder().build(),
 			runner: func(namespaceSelectors []string) *commonv1.Runner {
 				return createRunnerData(namespaceSelectors)
 			},
-			mockQuerierReturnAuth:	func(token *commonv1.AccessToken) (db.AuthAccessTokenRow, error) {
+			mockQuerierReturnAuth: func(token *commonv1.AccessToken) (db.AuthAccessTokenRow, error) {
 				return db.AuthAccessTokenRow{}, errors.New("Dummy Error")
 			},
 			mockQuerierReturnRunner: func(runner *commonv1.Runner) (db.Runner, error) {
-				runnerID, _ := uuid.Parse(runner.Id)
-				return db.Runner{
-					ID:                       runnerID,
-					Name:                     runner.Name,
-					NamespaceSelectors:       runner.NamespaceSelectors,
-					AcceptTestLabelSelectors: convertTestLabelSelectors(runner.AcceptTestLabelSelectors),
-					RejectTestLabelSelectors: convertTestLabelSelectors(runner.RejectTestLabelSelectors),
-					RegisteredAt:             types.FromProtoTimestampAsNullTime(runner.RegisteredAt),
-					LastHeartbeatAt:          types.FromProtoTimestampAsNullTime(runner.LastHeartbeatAt),
-				}, nil
+				return db.Runner{}, nil
 			},
 		},
 		{
-			name:         "Register runner returns error",
+			name:         "fail query for register runner",
 			responseCode: codes.Internal,
 			errMsg:       "failed to register runner",
 			token:        newAccessTokenBuilder().build(),
@@ -140,18 +131,18 @@ func TestRunnerServer_RegisterRunner(t *testing.T) {
 				}, nil
 			},
 			mockQuerierReturnRunner: func(runner *commonv1.Runner) (db.Runner, error) {
-				return db.Runner{}, errors.New("Runner Error")
+				return db.Runner{}, errors.New("Dummy Error")
 			},
 		},
 		{
-			name:         "Register runner without token",
+			name:         "register runner with auth token missing from context",
 			responseCode: codes.Internal,
 			errMsg:       "failed to get identity",
 			token:        nil,
 			runner: func(namespaceSelectors []string) *commonv1.Runner {
 				return createRunnerData(namespaceSelectors)
 			},
-			mockQuerierReturnAuth:  nil,
+			mockQuerierReturnAuth:   nil,
 			mockQuerierReturnRunner: nil,
 		},
 	}
@@ -160,24 +151,41 @@ func TestRunnerServer_RegisterRunner(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+
+			// if token is missing from context we expect the RegisterRunner call to fail before executing any DB query
+			if tt.token == nil {
+				ctx := context.Background()
+				_, err := server.RegisterRunner(ctx, &runnerv1.RegisterRunnerRequest{})
+				if er, ok := status.FromError(err); ok {
+					assert.Equal(t, er.Code(), tt.responseCode)
+					assert.Equal(t, er.Message(), tt.errMsg)
+				}
+				return
+			}
+
 			ctx := metadata.NewIncomingContext(context.Background(), metadata.Pairs(auth.MDKeyAuth, "bearer "+tt.token.Name))
+
 			tokenHash := auth.HashToken(tt.token.Name)
 			mockQuerier.EXPECT().AuthAccessToken(ctx, gomock.AssignableToTypeOf(server.pgxPool), tokenHash).Return(tt.mockQuerierReturnAuth(tt.token))
 
 			runner := tt.runner(tt.token.NamespaceSelectors)
-			mockQuerier.EXPECT().RegisterRunner(ctx, gomock.AssignableToTypeOf(server.pgxPool), db.RegisterRunnerParams{
-				Name:                     runner.Name,
-				NamespaceSelectors:       tt.token.NamespaceSelectors,
-				AcceptTestLabelSelectors: convertTestLabelSelectors(runner.AcceptTestLabelSelectors),
-				RejectTestLabelSelectors: convertTestLabelSelectors(runner.RejectTestLabelSelectors),
-			}).Return(tt.mockQuerierReturnRunner(runner))
+
+			// avoid setting an expectation for RegisterRunner call if there will not be any
+			if _, err := tt.mockQuerierReturnAuth(tt.token); err == nil {
+				mockQuerier.EXPECT().RegisterRunner(ctx, gomock.AssignableToTypeOf(server.pgxPool), db.RegisterRunnerParams{
+					Name:                     runner.Name,
+					NamespaceSelectors:       tt.token.NamespaceSelectors,
+					AcceptTestLabelSelectors: convertTestLabelSelectors(runner.AcceptTestLabelSelectors),
+					RejectTestLabelSelectors: convertTestLabelSelectors(runner.RejectTestLabelSelectors),
+				}).Return(tt.mockQuerierReturnRunner(runner))
+			}
 
 			request := &runnerv1.RegisterRunnerRequest{
 				Name:                     runner.Name,
 				AcceptTestLabelSelectors: runner.AcceptTestLabelSelectors,
 				RejectTestLabelSelectors: runner.RejectTestLabelSelectors}
 			res, err := server.RegisterRunner(ctx, request)
-			
+
 			if res != nil {
 				require.NoError(t, err)
 				assert.Equal(t, &runnerv1.RegisterRunnerResponse{Runner: runner}, res)
@@ -185,8 +193,8 @@ func TestRunnerServer_RegisterRunner(t *testing.T) {
 				if er, ok := status.FromError(err); ok {
 					assert.Equal(t, er.Code(), tt.responseCode)
 					assert.Equal(t, er.Message(), tt.errMsg)
-			}	
-			}								
+				}
+			}
 		})
 	}
 }

2683b44a0dfff1b28760ae0fe6b4a8a79139cb8f
Nan Zhong <me@nanzho.ng>
2022-11-25 15:33:38 -0500

    Add eglot config for handling go build tags

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,5 @@
 ;;; Directory Local Variables            -*- no-byte-compile: t -*-
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((magit-status-mode . ((magit-todos-exclude-globs . (".git/" "vendor/")))))
+((go-mode . ((eglot-workspace-configuration . (:gopls (:buildFlags ["-tags=integration,ui"])))))
+ (magit-status-mode . ((magit-todos-exclude-globs . (".git/" "vendor/")))))

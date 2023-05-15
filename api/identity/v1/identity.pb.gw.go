0a13ab504af5b674032eee17bcffb69ea14b6460
Nan Zhong <me@nanzho.ng>
2023-05-15 16:36:20 -0400

    Regenerate code

--- api/identity/v1/identity.pb.gw.go
+++ api/identity/v1/identity.pb.gw.go
@@ -86,7 +86,7 @@ func RegisterIdentityServiceHandlerServer(ctx context.Context, mux *runtime.Serv
 // RegisterIdentityServiceHandlerFromEndpoint is same as RegisterIdentityServiceHandler but
 // automatically dials to "endpoint" and closes the connection when "ctx" gets done.
 func RegisterIdentityServiceHandlerFromEndpoint(ctx context.Context, mux *runtime.ServeMux, endpoint string, opts []grpc.DialOption) (err error) {
-	conn, err := grpc.Dial(endpoint, opts...)
+	conn, err := grpc.DialContext(ctx, endpoint, opts...)
 	if err != nil {
 		return err
 	}
